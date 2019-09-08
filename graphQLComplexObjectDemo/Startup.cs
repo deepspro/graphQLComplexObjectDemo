4e47c0b89c03f917c6b43dfa839b113445692222
deepspro <deepalishinde29@gmail.com>
2019-09-08 12:28:59 +0530

    Call schema, query and type of graphQL in startup file

--- graphQLComplexObjectDemo/Startup.cs
+++ graphQLComplexObjectDemo/Startup.cs
@@ -1,6 +1,13 @@
+using GraphQL;
 using GraphQL.Server;
 using GraphQL.Server.Ui.Playground;
 using GraphQL.Types;
+using graphQLComplexObjectDemo.GraphQL.Queries;
+using graphQLComplexObjectDemo.GraphQL.Schemas;
+using graphQLComplexObjectDemo.GraphQL.Types;
+using graphQLComplexObjectDemo.Interface;
+using graphQLComplexObjectDemo.Models;
+using graphQLComplexObjectDemo.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -31,6 +38,17 @@ namespace graphQLComplexObjectDemo
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSingleton<IDependencyResolver>(s => new FuncDependencyResolver(s.GetRequiredService));
+            services.AddTransient<IBooksRepo, BooksRepo>();
+
+            services.AddSingleton<ISchema, ResponseSchema>();
+            services.AddSingleton<ResponseQuery>();
+            services.AddSingleton<ResponseType>();
+            services.AddSingleton<BookType>();
+            services.AddSingleton<AuthorType>();
+
+            services.AddGraphQL();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
